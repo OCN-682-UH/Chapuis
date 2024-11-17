89c8a541c576a8f5ae8e70f2ccd086a8df40203b
‘micachapuis’ <micachapuis@gmail.com>
2024-11-16 16:06:08 -1000

    w12 shiny app homework

--- W12-ShinyHomework/app.R
+++ W12-ShinyHomework/app.R
@@ -11,40 +11,81 @@ library(tidyverse)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Valentine's Day Consumer Data"),
+    titlePanel("Valentine's Day Consumer Data"), # adds a title at the top
+    
+    
+    # First set of sidebar + main panel
+    sidebarLayout( # type of layout
+        sidebarPanel( # panel in the sidebar
+          selectInput("select1", # input ID
+                      label = h3("Select Item to Plot by Year"), # title of the dropdown box
+                      choices = list("Candy", "Flowers", "Jewelry", "GreetingCards", "EveningOut", "Clothing", "GiftCards") # things to choose from (matches data column names)
+                      ),
+                    ),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
+        # Plot chosen data by year in main panel
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("linePlot")
         )
+    ),
+    
+    # Second set of sidebar + main panel
+    sidebarLayout( # type of layout
+      sidebarPanel( # panel in the sidebar
+        selectInput("select2", # input ID
+                    label = h3("Select Item to Plot by Gender"), # title of dropdown box
+                    choices = list("Candy", "Flowers", "Jewelry", "GreetingCards", "EveningOut", "Clothing", "GiftCards") # things to choose from (matches data column names)
+                    )
+                  ),
+      
+      # Plot chosen data by gender in main panel
+      mainPanel(
+        plotOutput("barPlot")
+      )
     )
 )
 
-# Define server logic required to draw a histogram
+
+# Define server
 server <- function(input, output) {
   
-  data <- reactive(readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-13/historical_spending.csv'))
+  ## Dynamic Figure
+  yearly_data <- reactive(readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-13/historical_spending.csv') %>%
+                     select(Year, Candy, Flowers, Jewelry, GreetingCards, EveningOut, Clothing, GiftCards) %>% # select columns I want to keep
+                     pivot_longer(!Year, names_to = "Item", values_to = "Value") %>% # pivot to long
+                     filter(Item %in% input$select1)) # filter to only keep the selected item (from the dropdown)
   
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  ### NOTE: all data cleaning/manipulation has to happen inside reactive() parentheses
+  
+  output$linePlot <- renderPlot({ # make plot
+        ggplot(yearly_data(), aes(x = Year, y = Value)) + # remember data is now a function so needs ()
+          geom_line() + # lines to connect the points
+          geom_point() + # the points
+          theme_minimal() + # do I still need to comment this
+          labs(y = "Average Amount Spent (USD)") + # y axis title
+          theme(axis.text = element_text(size = 12), # text sizes
+                axis.title = element_text(size = 14)) +
+      scale_x_continuous(breaks = seq(2010, 2022, by = 1)) # specify x axis labels (every 1 year)
     })
+  
+  
+  ## Additional Output
+  gender_data <- reactive(readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-13/gifts_gender.csv') %>%
+                          select(!SpendingCelebrating) %>% # remove extra column
+                          pivot_longer(!Gender, names_to = "Item", values_to = "Value") %>% # pivot long
+                          filter(Item %in% input$select2)) # choose item to plot from drop down
+  
+  
+  output$barPlot <- renderPlot({ # make plot 
+    ggplot(gender_data(), aes(x = Gender, y = Value, fill = Gender)) + # remember data is now a function so needs ()
+      geom_bar(stat = "identity") + # stat identity means I provide the y values from the data
+      theme_minimal() + 
+      labs(y = "Average Amount Spent (USD)") + # y axis title
+      theme(axis.text = element_text(size = 12), # text sizes
+            axis.title = element_text(size = 14)) + 
+      scale_fill_manual(values = c("#95b8d1", "#eac4d5"), guide = "none") # set colors for bars and remove legend
+    })
+  
 }
 
 # Run the application 
--- Week12/Scripts/Homework/w12_shinyhomework.md
+++ Week12/Scripts/Homework/w12_shinyhomework.md
@@ -7,15 +7,23 @@ In this document I will write the code that I will then use in my Shiny
 App, so I can explore the data and make sure my code works before
 running the app.
 
+**NOTE: Code is not commented, or cleaned. This is just for testing**
+
+You can find the final app at:
+<https://mchapuis.shinyapps.io/W12-ShinyHomework/>
+
 ## Assignment Details
 
 Make a shiny app: Use any data set to make a shiny app. Your app must
 have at least one dynamic figure and one other output (can be anything).
 You must publish it on shinyapps.io so that we can view your app
-online. - 3 points dynamic figure - 3 points additional output - 2
-points published (**add the link to your shiny app to your readme file
-so we can easily find it**) - 1 point - creativity - 1 point - clean and
-clear repository
+online.  
+- 3 points dynamic figure  
+- 3 points additional output  
+- 2 points published (**add the link to your shiny app to your readme
+file so we can easily find it**)  
+- 1 point - creativity  
+- 1 point - clean and clear repository
 
 ## Load Libraries
 
@@ -23,15 +31,67 @@ clear repository
 library(tidyverse)
 ```
 
-    ## ── Attaching core tidyverse packages ──────────────────────── tidyverse 2.0.0 ──
-    ## ✔ dplyr     1.1.4     ✔ readr     2.1.5
-    ## ✔ forcats   1.0.0     ✔ stringr   1.5.1
-    ## ✔ ggplot2   3.5.1     ✔ tibble    3.2.1
-    ## ✔ lubridate 1.9.3     ✔ tidyr     1.3.1
-    ## ✔ purrr     1.0.2     
-    ## ── Conflicts ────────────────────────────────────────── tidyverse_conflicts() ──
-    ## ✖ dplyr::filter() masks stats::filter()
-    ## ✖ dplyr::lag()    masks stats::lag()
-    ## ℹ Use the conflicted package (<http://conflicted.r-lib.org/>) to force all conflicts to become errors
-
 ## Load Data
+
+``` r
+data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-13/historical_spending.csv')
+```
+
+``` r
+data %>% select(Year, Candy, Flowers, Jewelry, GreetingCards, EveningOut, Clothing, GiftCards) %>%
+    pivot_longer(!Year, names_to = "Item", values_to = "Value") %>%
+    filter(Item == "Candy")
+```
+
+    ## # A tibble: 13 × 3
+    ##     Year Item  Value
+    ##    <dbl> <chr> <dbl>
+    ##  1  2010 Candy   8.6
+    ##  2  2011 Candy  10.8
+    ##  3  2012 Candy  10.8
+    ##  4  2013 Candy  11.6
+    ##  5  2014 Candy  10.8
+    ##  6  2015 Candy  12.7
+    ##  7  2016 Candy  13.1
+    ##  8  2017 Candy  12.7
+    ##  9  2018 Candy  13.1
+    ## 10  2019 Candy  14.1
+    ## 11  2020 Candy  17.3
+    ## 12  2021 Candy  15.3
+    ## 13  2022 Candy  15.9
+
+Plan:
+
+dynamic plot: line plot with year on the x axis, average amount spent
+per year on the y axis. dropdown where you choose which variable to plot
+(clothing, giftcards, candy, flowers, etc)
+
+``` r
+data %>% ggplot(aes(x = Year, y = Candy)) +
+  geom_line() +
+  geom_point() +
+  theme_minimal()
+```
+
+![](w12_shinyhomework_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+
+## Additional item
+
+``` r
+gifts_gender <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-13/gifts_gender.csv') %>%
+                          select(!SpendingCelebrating) %>%
+                          pivot_longer(!Gender, names_to = "Item", values_to = "Value") %>%
+                          filter(Item %in% "Candy")
+```
+
+``` r
+    ggplot(gifts_gender, aes(x = Gender, y = Value, fill = Gender)) + 
+      geom_bar(stat = "identity") + 
+      theme_minimal() + 
+      labs(y = "Average Amount Spent (USD)") +
+      theme(axis.text = element_text(size = 12),
+            axis.title = element_text(size = 14)) +
+      scale_fill_manual(values = c("#95b8d1", "#eac4d5"))
+```
+
+![](w12_shinyhomework_files/figure-gfm/unnamed-chunk-6-1.png)<!-- -->
